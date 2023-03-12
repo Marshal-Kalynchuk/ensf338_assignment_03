88c96338fa6b1af84ef767e4eda0003ce27d7f3b
Marshal <marshalkalynchuk1@gmail.com>
2023-03-11 21:16:17 -0700

    Fixed ex3.2.py

--- ex3.2.py
+++ ex3.2.py
@@ -1,14 +1,13 @@
 import json
+from urllib.request import urlopen
 import timeit
 import matplotlib.pyplot as plt
 
+with urlopen('https://raw.githubusercontent.com/ldklab/ensf338w23/main/assignments/assignment3/ex2data.json') as inUrl:
+    data = json.load(inUrl)
 
-# Load the array and the list of search tasks
-with open("ex2data.json", "r") as f:
-    data = json.load(f)
-
-with open("ex2tasks.json", "r") as f:
-    tasks = json.load(f)
+with urlopen('https://raw.githubusercontent.com/ldklab/ensf338w23/main/assignments/assignment3/ex2tasks.json') as inUrl:
+    tasks = json.load(inUrl)
 
 def binary_search(array, target, start=0, end=None, initial_midpoint=None):
     """Perform binary search on array to find target.
@@ -34,28 +33,31 @@ def binary_search(array, target, start=0, end=None, initial_midpoint=None):
 
     return False
     
-results = []
-for task in tasks:
-
-    t = timeit.Timer(lambda: binary_search(data, task, initial_midpoint=task))
-    elapsed_time = t.timeit(number=10)
-    results.append((task, elapsed_time))
 
 
-# Extract task and elapsed time values from results
-tasks, elapsed_times = zip(*results)
+times = []
+times = [timeit.timeit(lambda: binary_search(data, task, initial_midpoint=task), number=100) for task in tasks]
 
 # Create new figure
 fig = plt.figure(figsize=(8, 6))
-ax = fig.add_subplot(1, 1, 1)
+
 
 # Plot data
-ax.scatter(tasks, elapsed_times)
+plt.subplot(1, 2, 1)
+plt.scatter(range(len(tasks)), times)
 
 # Add labels and title
-ax.set_xlabel("Search Task")
-ax.set_ylabel("Elapsed Time (s)")
-ax.set_title("Binary Search Performance")
+plt.xlabel("Search Task")
+plt.ylabel("Elapsed Time (s)")
+plt.title("Elapsed binary search time per task")
+
+plt.subplot(1, 2, 2)
+plt.scatter(range(len(tasks)), tasks)
+# Add labels and title
+plt.xlabel("Search Task")
+plt.ylabel("Chosen midpoint")
+plt.title("Chosen midpoint per task")
+
 
 # Show plot
 plt.show()
