cfd1c28672348031faef0a976b541b4dbb831e8c
Marshal <marshalkalynchuk1@gmail.com>
2023-03-11 20:34:51 -0700

    Finished ex3.2.py

--- ex3.2.py
+++ ex3.2.py
@@ -1,47 +1,61 @@
 import json
-import time
+import timeit
 import matplotlib.pyplot as plt
 
+
+# Load the array and the list of search tasks
 with open("ex2data.json", "r") as f:
     data = json.load(f)
 
 with open("ex2tasks.json", "r") as f:
     tasks = json.load(f)
 
-def binary_search(arr, target, start, end, mid):
-    if start > end:
-        return False
-    if target == arr[mid]:
-        return True
-    elif target < arr[mid]:
-        return binary_search(arr, target, start, mid-1, (start+mid-1)//2)
-    else:
-        return binary_search(arr, target, mid+1, end, (mid+1+end)//2)
+def binary_search(array, target, start=0, end=None, initial_midpoint=None):
+    """Perform binary search on array to find target.
+    Assumes array is sorted.
+    Returns True if target is found, False otherwise.
+    """
+    if end is None:
+        end = len(array) - 1
+
+    while start <= end:
+        if initial_midpoint is None:
+            mid = start + (end - start) // 2
+        else:
+            mid = initial_midpoint
+            initial_midpoint = None
+        
+        if array[mid] == target:
+            return True
+        elif array[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return False
     
 results = []
-
 for task in tasks:
-    start = 0
-    end = len(data) - 1
-    target = task
-    best_mid = (start+end)//2
-    min_time = float('inf')
-    
-    for mid in range(start, end+1):
-        start_time = time.time()
-        found = binary_search(data, target, start, end, mid)
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        if elapsed_time < min_time:
-            min_time = elapsed_time
-            best_mid = mid
-    
-    print((target, best_mid))
-    results.append((target, best_mid))
+
+    t = timeit.Timer(lambda: binary_search(data, task, initial_midpoint=task))
+    elapsed_time = t.timeit(number=10)
+    results.append((task, elapsed_time))
 
 
-plt.scatter([x[0] for x in results], [x[1] for x in results])
-plt.title("Initial Midpoints for Binary Search")
-plt.xlabel("Search Task")
-plt.ylabel("Initial Midpoint")
+# Extract task and elapsed time values from results
+tasks, elapsed_times = zip(*results)
+
+# Create new figure
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(1, 1, 1)
+
+# Plot data
+ax.scatter(tasks, elapsed_times)
+
+# Add labels and title
+ax.set_xlabel("Search Task")
+ax.set_ylabel("Elapsed Time (s)")
+ax.set_title("Binary Search Performance")
+
+# Show plot
 plt.show()
